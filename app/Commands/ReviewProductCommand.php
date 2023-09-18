4dfe227f4f5410f029e8b2616fc6da0897a33174
Arbha Pradana <arbhapr@gmail.com>
2023-09-18 14:35:13 +0700

    feat(reviews): add review-product and review-summary console

---
+++ app/Commands/ReviewProductCommand.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Commands;
+
+use Illuminate\Support\Facades\Cache;
+use LaravelZero\Framework\Commands\Command;
+
+class ReviewProductCommand extends Command
+{
+    /**
+     * The signature of the command.
+     *
+     * @var string
+     */
+    protected $signature = 'review:product {productId : Id for product}';
+
+    /**
+     * The description of the command.
+     *
+     * @var string
+     */
+    protected $description = 'List review of product';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $productId = $this->argument('productId');
+        $cacheKey = 'product-' . $productId;
+
+        if (Cache::has($cacheKey)) {
+            dd('from-cache', Cache::get($cacheKey));
+        }
+
+        $ratings = [
+            1 => 0,
+            2 => 0,
+            3 => 0,
+            4 => 0,
+            5 => 0,
+        ];
+        $rawReviews = json_decode(file_get_contents(storage_path() . "/reviews.json"), true);
+        $rawProducts = json_decode(file_get_contents(storage_path() . "/products.json"));
+
+        $reviews = collect($rawReviews)->where('product_id', $productId);
+        foreach ($reviews as $n => $item) {
+            if ($item['rating'] == 1) $ratings[1] += 1;
+            elseif ($item['rating'] == 2) $ratings[2] += 1;
+            elseif ($item['rating'] == 3) $ratings[3] += 1;
+            elseif ($item['rating'] == 4) $ratings[4] += 1;
+            elseif ($item['rating'] == 5) $ratings[5] += 1;
+        }
+
+        $total_review = ($ratings[1] * 1) + ($ratings[2] * 2) + ($ratings[3] * 3) + ($ratings[4] * 4) + ($ratings[5] * 5);
+
+        $response = (object) [
+            'avg_reviews' => (count($reviews) < 0) ? number_format($total_review / count($reviews), 2) : 0,
+            'total_reviews' => count($reviews),
+            'review_per_rating' => $ratings,
+        ];
+        Cache::put($cacheKey, $response, 180);
+        dd('not-from-cache', $response);
+    }
+}
